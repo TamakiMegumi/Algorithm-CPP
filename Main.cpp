d64ad6c00ad82db418f700f41d97c4c0a31ebd02
AzabuMai <mgwk1120@outlook.com>
2026-04-14 21:34:49 +0800

    initial

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+﻿// Algorithm.cpp: 定义应用程序的入口点。
+//
+
+#include "Algorithm.h"
+using std::cin, std::cout, std::cerr, std::clog, std::endl;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << 1 << endl;
+	return 0;
+}
